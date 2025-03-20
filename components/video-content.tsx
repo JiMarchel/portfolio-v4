2c216d495d96162be2e62e19d1b8f26ebcca5fc5
Jimmy Marchel <jmarchel100@gmail.com>
2025-03-20 14:49:47 +0700

    fix failed deploy

--- components/video-content.tsx
+++ components/video-content.tsx
@@ -1,13 +1,18 @@
 interface VideoContent {
   video: string
 }
+
 export const VideoContent = ({ video }: VideoContent) => {
   return (
     <video width={340} height={240} autoPlay>
       <source src={video} type="video/mp4" />
-      <track kind="caption" />
+      <track
+        kind="captions"
+        srcLang="en"
+        label="English"
+        src=""
+      />
       Your browser does not support the video tag.
     </video>
   )
 }
-
