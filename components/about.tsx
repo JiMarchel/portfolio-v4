f0be59ccfdfa0c861b765c22c805c3921eddc136
JiMarchel <jmarchel100@gmail.com>
2025-06-16 23:48:45 +0700

    remove live website

--- components/about.tsx
+++ components/about.tsx
@@ -1,103 +1,236 @@
-"use client"
-import { lilitaOne, rowdies } from '@/config/fonts'
-import { Button, ButtonGroup, Card, CardBody, CardFooter, CardHeader, Image, Link, Popover, PopoverContent, PopoverTrigger, Tooltip } from '@heroui/react'
-import { Download, Facebook, Instagram, Linkedin, Mail } from 'lucide-react'
-import React from 'react'
-import { GithubIcon } from './icons'
-import { siteConfig } from '@/config/site'
+"use client";
+import { lilitaOne, rowdies } from "@/config/fonts";
+import {
+    Button,
+    ButtonGroup,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    Image,
+    Link,
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+    Tooltip,
+} from "@heroui/react";
+import { Download, Facebook, Instagram, Linkedin, Mail } from "lucide-react";
+import React from "react";
+import { GithubIcon } from "./icons";
+import { siteConfig } from "@/config/site";
 
 const About = () => {
-  return (
-    <div className='space-y-12'>
-      <div className='flex flex-col gap-4'>
-        <div className='flex gap-2'>
-          <h1 className='font-bold text-2xl sm:text-4xl '>Haiii again, I&apos;m
-            <Tooltip content={
-              <Image alt='M Jimmy Marchel' src='/me.webp' isBlurred className='col-span-1 m-2 max-h-96' />} placement='bottom'>
-              <span className={`${lilitaOne.className} border-b-2 border-purple-800 hidden sm:inline-block ml-3 cursor-default`}>Jimmy.</span>
-            </Tooltip>
-          </h1>
-          <Popover >
-            <PopoverTrigger>
-              <span className={`${lilitaOne.className} font-bold text-2xl sm:text-4xl  border-b-2 border-purple-800 sm:hidden cursor-default w-fit`}>Jimmy.</span>
-            </PopoverTrigger>
-            <PopoverContent>
-              <Image alt='M Jimmy Marchel' src='/me.webp' isBlurred className='col-span-1 m-2 max-h-96' />
-            </PopoverContent>
-          </Popover>
-        </div>
-        <p className='font-medium text-lg sm:text-xl '>I am Mochammad Jimmy Marchel, a 6th-semester Informatics Engineering student at Muhammadiyah University of Sidoarjo, with 3 years of hands-on experience in self-taught web development. My journey has centered on honing full-stack development skills while actively exploring cutting-edge technologies and frameworks. Driven by a growth mindset, I thrive on learning new thing. </p>
-        <div>
-          <p className='font-bold text-lg sm:text-xl '>Get in touch</p>
-          <ButtonGroup>
-            <Tooltip content="Instagram" color="danger">
-              <Button color='danger' variant='faded' isIconOnly as={Link} target='_blank' href={siteConfig.links.instagram}><Instagram /></Button>
-            </Tooltip>
-            <Tooltip content="Facebook" color='primary' placement='bottom'>
-              <Button color='primary' variant='faded' isIconOnly as={Link} target='_blank' href={siteConfig.links.facebook} ><Facebook /></Button>
-            </Tooltip>
-            <Tooltip content="Github">
-              <Button variant='faded' isIconOnly as={Link} target='_blank' href={siteConfig.links.github}><GithubIcon /></Button>
-            </Tooltip>
-            <Tooltip content="LinkedIn" color='primary' placement='bottom'>
-              <Button color='primary' variant='faded' isIconOnly as={Link} target='_blank' href={siteConfig.links.linkedin}><Linkedin /></Button>
-            </Tooltip>
-            <Tooltip content="Gmail" color='danger'>
-              <Button color='danger' variant='faded' isIconOnly as={Link} target='_blank' href={`mailto:${siteConfig.links.email}`} ><Mail /></Button>
-            </Tooltip>
-          </ButtonGroup>
-        </div>
-        <div >
-          <p className='font-bold text-lg sm:text-xl'>CV</p>
-          <Tooltip content="My CV" placement='bottom-start'>
-            <Button startContent={<Download />} color='secondary' variant='flat' as={Link} target='_blank' href='/CV.pdf' >Download CV</Button>
-          </Tooltip>
-        </div>
-      </div>
+    return (
+        <div className="space-y-12">
+            <div className="flex flex-col gap-4">
+                <div className="flex gap-2">
+                    <h1 className="font-bold text-2xl sm:text-4xl ">
+                        Haiii again, I&apos;m
+                        <Tooltip
+                            content={
+                                <Image
+                                    alt="M Jimmy Marchel"
+                                    src="/me.webp"
+                                    isBlurred
+                                    className="col-span-1 m-2 max-h-96"
+                                />
+                            }
+                            placement="bottom"
+                        >
+                            <span
+                                className={`${lilitaOne.className} border-b-2 border-purple-800 hidden sm:inline-block ml-3 cursor-default`}
+                            >
+                                Jimmy.
+                            </span>
+                        </Tooltip>
+                    </h1>
+                    <Popover>
+                        <PopoverTrigger>
+                            <span
+                                className={`${lilitaOne.className} font-bold text-2xl sm:text-4xl  border-b-2 border-purple-800 sm:hidden cursor-default w-fit`}
+                            >
+                                Jimmy.
+                            </span>
+                        </PopoverTrigger>
+                        <PopoverContent>
+                            <Image
+                                alt="M Jimmy Marchel"
+                                src="/me.webp"
+                                isBlurred
+                                className="col-span-1 m-2 max-h-96"
+                            />
+                        </PopoverContent>
+                    </Popover>
+                </div>
+                <p className="font-medium text-lg sm:text-xl ">
+                    I am Mochammad Jimmy Marchel, a Informatics Engineering
+                    student at Muhammadiyah University of Sidoarjo, with 3 years
+                    of hands-on experience in self-taught web development. My
+                    journey has centered on honing full-stack development skills
+                    while actively exploring cutting-edge technologies and
+                    frameworks. Driven by a growth mindset, I thrive on learning
+                    new thing.{" "}
+                </p>
+                <div>
+                    <p className="font-bold text-lg sm:text-xl ">
+                        Get in touch
+                    </p>
+                    <ButtonGroup>
+                        <Tooltip content="Instagram" color="danger">
+                            <Button
+                                color="danger"
+                                variant="faded"
+                                isIconOnly
+                                as={Link}
+                                target="_blank"
+                                href={siteConfig.links.instagram}
+                            >
+                                <Instagram />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip
+                            content="Facebook"
+                            color="primary"
+                            placement="bottom"
+                        >
+                            <Button
+                                color="primary"
+                                variant="faded"
+                                isIconOnly
+                                as={Link}
+                                target="_blank"
+                                href={siteConfig.links.facebook}
+                            >
+                                <Facebook />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip content="Github">
+                            <Button
+                                variant="faded"
+                                isIconOnly
+                                as={Link}
+                                target="_blank"
+                                href={siteConfig.links.github}
+                            >
+                                <GithubIcon />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip
+                            content="LinkedIn"
+                            color="primary"
+                            placement="bottom"
+                        >
+                            <Button
+                                color="primary"
+                                variant="faded"
+                                isIconOnly
+                                as={Link}
+                                target="_blank"
+                                href={siteConfig.links.linkedin}
+                            >
+                                <Linkedin />
+                            </Button>
+                        </Tooltip>
+                        <Tooltip content="Gmail" color="danger">
+                            <Button
+                                color="danger"
+                                variant="faded"
+                                isIconOnly
+                                as={Link}
+                                target="_blank"
+                                href={`mailto:${siteConfig.links.email}`}
+                            >
+                                <Mail />
+                            </Button>
+                        </Tooltip>
+                    </ButtonGroup>
+                </div>
+                <div>
+                    <p className="font-bold text-lg sm:text-xl">CV</p>
+                    <Tooltip content="My CV" placement="bottom-start">
+                        <Button
+                            startContent={<Download />}
+                            color="secondary"
+                            variant="flat"
+                            as={Link}
+                            target="_blank"
+                            href="/CV.pdf"
+                        >
+                            Download CV
+                        </Button>
+                    </Tooltip>
+                </div>
+            </div>
 
-      <div className='space-y-3'>
-        <h1 className='font-bold text-2xl sm:text-4xl'>Cert</h1>
-        <div className='grid grid-cols-2'>
-          {siteConfig.cert.map((v, i) => (
-            <Card key={i} isPressable shadow='sm'>
-              <CardHeader>
-                <Image
-                  alt={v.companyName}
-                  className="object-cover h-auto w-auto"
-                  radius="lg"
-                  shadow="sm"
-                  src={v.image}
-                />
-              </CardHeader>
-              <CardBody >
-                <h4 className='font-medium sm:font-bold text-lg sm:text-xl'>{v.companyName}</h4>
-              </CardBody>
-              <CardFooter className='grid gap-1'>
-                <p className='text-start text-sm sm:text-lg'>{v.description}</p>
-                <Button as={Link} variant='flat' size='sm' startContent={<Download size={14} />} target='_blank' href={v.pdf} download={`${v.companyName} - Moch Jimmy Marchel`} className=''>PDF</Button>
-              </CardFooter>
-            </Card>
-          ))}
-        </div>
-      </div>
+            <div className="space-y-3">
+                <h1 className="font-bold text-2xl sm:text-4xl">Cert</h1>
+                <div className="grid grid-cols-2">
+                    {siteConfig.cert.map((v, i) => (
+                        <Card key={i} isPressable shadow="sm">
+                            <CardHeader>
+                                <Image
+                                    alt={v.companyName}
+                                    className="object-cover h-auto w-auto"
+                                    radius="lg"
+                                    shadow="sm"
+                                    src={v.image}
+                                />
+                            </CardHeader>
+                            <CardBody>
+                                <h4 className="font-medium sm:font-bold text-lg sm:text-xl">
+                                    {v.companyName}
+                                </h4>
+                            </CardBody>
+                            <CardFooter className="grid gap-1">
+                                <p className="text-start text-sm sm:text-lg">
+                                    {v.description}
+                                </p>
+                                <Button
+                                    as={Link}
+                                    variant="flat"
+                                    size="sm"
+                                    startContent={<Download size={14} />}
+                                    target="_blank"
+                                    href={v.pdf}
+                                    download={`${v.companyName} - Moch Jimmy Marchel`}
+                                    className=""
+                                >
+                                    PDF
+                                </Button>
+                            </CardFooter>
+                        </Card>
+                    ))}
+                </div>
+            </div>
 
-      <div className='space-y-3'>
-        <h1 className='font-bold text-2xl sm:text-4xl'>Listening</h1>
-        <div className='grid grid-cols-2 sm:grid-cols-3 gap-2'>
-          {siteConfig.listening.map((v, i) => (
-            <Card key={i} shadow='lg' as={Link} href={v.link} target="_blank" className='-rotate-3 hover:rotate-0'>
-              <CardBody>
-                <Image src={v.image} alt={v.title} className='object-cover h-auto w-auto' />
-              </CardBody>
-              <CardFooter className={`${rowdies.className}`}>
-                {v.title}
-              </CardFooter>
-            </Card>
-          ))}
+            <div className="space-y-3">
+                <h1 className="font-bold text-2xl sm:text-4xl">Listening</h1>
+                <div className="grid grid-cols-2 sm:grid-cols-3 gap-2">
+                    {siteConfig.listening.map((v, i) => (
+                        <Card
+                            key={i}
+                            shadow="lg"
+                            as={Link}
+                            href={v.link}
+                            target="_blank"
+                            className="-rotate-3 hover:rotate-0"
+                        >
+                            <CardBody>
+                                <Image
+                                    src={v.image}
+                                    alt={v.title}
+                                    className="object-cover h-auto w-auto"
+                                />
+                            </CardBody>
+                            <CardFooter className={`${rowdies.className}`}>
+                                {v.title}
+                            </CardFooter>
+                        </Card>
+                    ))}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
-}
+    );
+};
 
-export default About
+export default About;
--- config/site.ts
+++ config/site.ts
@@ -3,152 +3,161 @@ import { RiNextjsFill, RiReactjsLine } from "react-icons/ri";
 import { FaGolang, FaRust } from "react-icons/fa6";
 export type SiteConfig = typeof siteConfig;
 
-
 export const siteConfig = {
-  name: "JiMarchel",
-  description: "Mochammad Jimmy Marchel Personal Website Portofolio",
-  navItems: [
-    {
-      label: "Projects",
-      icon: Folder,
-      href: "/",
+    name: "JiMarchel",
+    description: "Mochammad Jimmy Marchel Personal Website Portofolio",
+    navItems: [
+        {
+            label: "Projects",
+            icon: Folder,
+            href: "/",
+        },
+        {
+            label: "Now",
+            icon: Clock,
+            href: "/now",
+        },
+        {
+            label: "About",
+            icon: Smile,
+            href: "/about",
+        },
+    ],
+    listProjects: [
+        {
+            title: "KKN-Bmi",
+            description:
+                "A bmi calculator that I made to calculate the ideal weight of people in Sumbersuko village",
+            github: "https://github.com/JiMarchel/kkn-bmi",
+            // live: "https://bmi-sumbersuko.vercel.app/",
+            live: null,
+            icon: RiNextjsFill,
+        },
+        {
+            title: "Al-Quran",
+            description: "A website for reading the Al-Quran and short surahs",
+            github: "https://github.com/JiMarchel/alquran",
+            // live: "https://alquran-035.vercel.app/",
+            live: null,
+            icon: RiNextjsFill,
+        },
+        {
+            title: "MyKartunList",
+            description:
+                "MyAnimeList lite, Build this with NextJs and use NextUi for ui components. Data API from jikan.moe",
+            github: "https://github.com/JiMarchel/MyKartunList",
+            // live: "https://mykartunlist.vercel.app",
+            live: null,
+            icon: RiNextjsFill,
+        },
+        {
+            title: "TMDB-Movie",
+            description:
+                "A movie collection website where you can view information about the movie. API from TMDb",
+            github: "https://github.com/JiMarchel/tmdb-movie",
+            // live: "https://tmdb-mymy.vercel.app",
+            live: null,
+            icon: RiNextjsFill,
+        },
+        {
+            title: "Weather CLI",
+            description: "Generate upcoming weather predictions on your cli.",
+            github: "https://github.com/JiMarchel/weather",
+            live: null,
+            icon: FaGolang,
+        },
+        {
+            title: "Mac Changer",
+            description: "Generate random mac address for you.",
+            github: "https://github.com/JiMarchel/mac-changer",
+            live: null,
+            icon: FaRust,
+        },
+        {
+            title: "HooBank UI",
+            description:
+                "Slicing design with Reactjs and Tailwind from following JSMastery course.",
+            github: "https://github.com/JiMarchel/HooBank",
+            // live: "https://keen-arithmetic-8a9459.netlify.app/",
+            live: null,
+            icon: RiReactjsLine,
+        },
+        {
+            title: "Taskify",
+            description:
+                "Taskify, the Trello clone i made from following @CodeWIthAntonio Course. learn so much from this course.",
+            github: "https://github.com/JiMarchel/Taskify",
+            live: null,
+            icon: RiNextjsFill,
+        },
+    ],
+    newAt: [
+        {
+            title: "Rust",
+            image: "/rust.jpg",
+        },
+        {
+            title: "Docker",
+            image: "/docker.png",
+        },
+    ],
+    masterAt: [
+        {
+            title: "Typescript",
+            image: "/ts.png",
+        },
+        {
+            title: "NodeJS",
+            image: "/nodejs.jpg",
+        },
+        {
+            title: "NextJS",
+            image: "/nextjs.png",
+        },
+        {
+            title: "Bun",
+            image: "/bun.jpg",
+        },
+        {
+            title: "SQL Database",
+            image: "/sql.png",
+        },
+        {
+            title: "Linux",
+            image: "linux.png",
+        },
+    ],
+    cert: [
+        {
+            companyName: "Bisa AI Academy",
+            description:
+                "The independent study program that I joined at Bisa AI Academy, I took the cyber security position.",
+            image: "/cert/bisa_ai.png",
+            pdf: "/cert/bisa_ai.pdf",
+        },
+    ],
+    listening: [
+        {
+            title: "Aespa",
+            image: "/listening/aespa.jpg",
+            link: "https://open.spotify.com/artist/6YVMFz59CuY7ngCxTxjpxE?si=tKYzok1YQl65jjvrfNZvdQ",
+        },
+        {
+            title: "Malaka Project",
+            image: "/listening/malaka.jpg",
+            link: "https://open.spotify.com/show/1ZbLoxAjgM9Sn43oPKvhw3?si=869ad8f2fdac4ffe",
+        },
+        {
+            title: "DVRST",
+            image: "/listening/dvrst.jpg",
+            link: "https://open.spotify.com/artist/0XFgyr4jwM0MGeZZW0VzA5?si=bpj679QnRRWRFncN05FRyg",
+        },
+    ],
+    links: {
+        github: "https://github.com/jimarchel",
+        facebook: "https://www.facebook.com/jimarchel",
+        instagram: "https://www.instagram.com/jimarchel",
+        linkedin: "https://www.linkedin.com/in/jimarchel",
+        email: "dev@jimarchel.my.id",
     },
-    {
-      label: "Now",
-      icon: Clock,
-      href: "/now",
-    },
-    {
-      label: "About",
-      icon: Smile,
-      href: "/about",
-    },
-  ],
-  listProjects: [
-    {
-      title: "KKN-Bmi",
-      description: "A bmi calculator that I made to calculate the ideal weight of people in Sumbersuko village",
-      github: "https://github.com/JiMarchel/kkn-bmi",
-      live: "https://bmi-sumbersuko.vercel.app/",
-      icon: RiNextjsFill
-    },
-    {
-      title: "Al-Quran",
-      description: "A website for reading the Al-Quran and short surahs",
-      github: "https://github.com/JiMarchel/alquran",
-      live: "https://alquran-035.vercel.app/",
-      icon: RiNextjsFill
-    },
-    {
-      title: "MyKartunList",
-      description: "MyAnimeList lite, Build this with NextJs and use NextUi for ui components. Data API from jikan.moe",
-      github: "https://github.com/JiMarchel/MyKartunList",
-      live: "https://mykartunlist.vercel.app",
-      icon: RiNextjsFill
-    },
-    {
-      title: "TMDB-Movie",
-      description: "A movie collection website where you can view information about the movie. API from TMDb",
-      github: "https://github.com/JiMarchel/tmdb-movie",
-      live: "https://tmdb-mymy.vercel.app",
-      icon: RiNextjsFill
-    },
-    {
-      title: "Weather CLI",
-      description: "Generate upcoming weather predictions on your cli.",
-      github: "https://github.com/JiMarchel/weather",
-      live: null,
-      icon: FaGolang
-    },
-    {
-      title: "Mac Changer",
-      description: "Generate random mac address for you.",
-      github: "https://github.com/JiMarchel/mac-changer",
-      live: null,
-      icon: FaRust
-    },
-    {
-      title: "HooBank UI",
-      description: "Slicing design with Reactjs and Tailwind from following JSMastery course.",
-      github: "https://github.com/JiMarchel/HooBank",
-      live: "https://keen-arithmetic-8a9459.netlify.app/",
-      icon: RiReactjsLine
-    },
-    {
-      title: "Taskify",
-      description: "Taskify, the Trello clone i made from following @CodeWIthAntonio Course. learn so much from this course.",
-      github: "https://github.com/JiMarchel/Taskify",
-      live: null,
-      icon: RiNextjsFill
-    },
-
-  ],
-  newAt: [
-    {
-      title: "Rust",
-      image: "/rust.jpg"
-    },
-    {
-      title: "Docker",
-      image: "/docker.png"
-    }
-  ],
-  masterAt: [
-    {
-      title: "Typescript",
-      image: "/ts.png"
-    },
-    {
-      title: "NodeJS",
-      image: "/nodejs.jpg"
-    },
-    {
-      title: "NextJS",
-      image: "/nextjs.png"
-    },
-    {
-      title: "Bun",
-      image: "/bun.jpg"
-    },
-    {
-      title: "SQL Database",
-      image: "/sql.png"
-    },
-    {
-      title: "Linux",
-      image: "linux.png"
-    }
-  ],
-  cert: [
-    {
-      companyName: "Bisa AI Academy",
-      description: "The independent study program that I joined at Bisa AI Academy, I took the cyber security position.",
-      image: "/cert/bisa_ai.png",
-      pdf: "/cert/bisa_ai.pdf",
-    }
-  ],
-  listening: [
-    {
-      title: "Aespa",
-      image: "/listening/aespa.jpg",
-      link: "https://open.spotify.com/artist/6YVMFz59CuY7ngCxTxjpxE?si=tKYzok1YQl65jjvrfNZvdQ"
-    },
-    {
-      title: "Malaka Project",
-      image: "/listening/malaka.jpg",
-      link: "https://open.spotify.com/show/1ZbLoxAjgM9Sn43oPKvhw3?si=869ad8f2fdac4ffe"
-    },
-    {
-      title: "DVRST",
-      image: "/listening/dvrst.jpg",
-      link: "https://open.spotify.com/artist/0XFgyr4jwM0MGeZZW0VzA5?si=bpj679QnRRWRFncN05FRyg"
-    },
-  ],
-  links: {
-    github: "https://github.com/jimarchel",
-    facebook: "https://www.facebook.com/jimarchel",
-    instagram: "https://www.instagram.com/jimarchel",
-    linkedin: "https://www.linkedin.com/in/jimarchel",
-    email: "jmarchel100@gmail.com",
-  },
 };
