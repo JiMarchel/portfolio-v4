cca4a79ff22c418f95b30abc092f63ac6bb62755
jimarchel <jmarchel100@gmail.com>
2025-08-15 15:48:53 +0700

    Add Tools component to display skills and technologies with animated marquee

---
+++ components/tools.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { siteConfig } from "@/config/site";
+import { Marquee } from "./magicui/marquee";
+import Image from "next/image";
+
+export const Tools = () => {
+  return (
+    <div>
+      <h1 className="font-medium text-xl sm:text-2xl">
+        🛠️⚙️Tools and Technologies
+      </h1>
+      <p className="text-muted-foreground">*Still beginner at Rust.</p>
+      <Marquee pauseOnHover className="[--duration:20s]">
+        {siteConfig.skills.map((v) => (
+          <div key={v.title} className="flex items-center gap-2 ml-5 sm:ml-9">
+            <Image
+              src={v.image}
+              alt={v.title}
+              className="h-7 opacity-70 w-auto"
+              width={30}
+              height={200}
+            />
+            <p className="text-xl">{v.title}</p>
+          </div>
+        ))}
+      </Marquee>
+      <Marquee pauseOnHover reverse className="[--duration:20s]">
+        {siteConfig.toolsAndSkills.map((v) => (
+          <div key={v.title} className="flex items-center gap-2 ml-5 sm:ml-9">
+            <Image
+              src={v.image}
+              alt={v.title}
+              className="h-7 opacity-70 w-auto"
+              width={30}
+              height={200}
+            />
+            <p className="text-xl">{v.title}</p>
+          </div>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
