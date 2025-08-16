2a071b759d6e31a10ce5a2dc97e82b3f93b0edb5
jimarchel <jmarchel100@gmail.com>
2025-08-16 13:52:32 +0700

    Add SiteLayout component for main application structure

---
+++ app/(site)/layout.tsx
@@ -0,0 +1,56 @@
+import { Footer } from "@/components/footer";
+import { SanityLive } from "@/sanity/lib/live";
+import { VisualEditing } from "next-sanity";
+import { draftMode } from "next/headers";
+import { Providers } from "../providers";
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import clsx from "clsx";
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/witch-hat.png",
+    apple: "/witch-hat.png",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+};
+
+export default async function SiteLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "h-full bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <main className="container h-full mx-auto max-w-2xl pt-12 sm:pt-24 px-6 flex-grow">
+            {children}
+            <SanityLive />
+            {(await draftMode()).isEnabled && <VisualEditing />}
+          </main>
+          <Footer />
+        </Providers>
+      </body>
+    </html>
+  );
+}
