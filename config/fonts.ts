6b7a250c8646a524b3d9af3ad7dbd0d70d31782a
jimarchel <jmarchel100@gmail.com>
2025-08-17 20:53:06 +0700

    Refactor font configuration by removing unused font imports and updating FunSized font settings

--- config/fonts.ts
+++ config/fonts.ts
@@ -1,53 +1,16 @@
-import localFont from "next/font/local"
-import { Fira_Code as FontMono, Source_Sans_3 as FontSans, Barriecito, Press_Start_2P, Rubik_Burned, Grape_Nuts, Lilita_One, Rowdies } from "next/font/google";
+import localFont from "next/font/local";
+import { Source_Sans_3 as FontSans } from "next/font/google";
 
 export const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
+  preload: false,
 });
-
-export const fontMono = FontMono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-});
-
-export const barriecito = Barriecito({
-  subsets: ["latin"],
-  variable: "--barriecito",
-  weight: "400"
-})
-
-export const pressStart2P = Press_Start_2P({
-  subsets: ["latin"],
-  variable: "--pressStart2P",
-  weight: "400"
-})
-
-export const rubikBurned = Rubik_Burned({
-  subsets: ["latin"],
-  variable: "--rubikBurned",
-  weight: "400"
-})
-
-export const grapeNuts = Grape_Nuts({
-  subsets: ["latin"],
-  variable: "--grape-nuts",
-  weight: "400"
-})
-
-export const lilitaOne = Lilita_One({
-  subsets: ["latin"],
-  variable: "--lilita-one",
-  weight: "400"
-})
-
-export const rowdies = Rowdies({
-  subsets: ["latin"],
-  variable: "--rowdies",
-  weight: "400"
-})
-
 
 export const funSized = localFont({
-  src: "./FunSized.ttf"
-})
+  src: "./FunSized.woff2",
+  weight: "400",
+  display: "swap",
+  preload: false,
+  variable: "--font-fun-sized",
+});
