9ec5039c1a6ee0baa2b78670d5232048a209c5dd
jimarchel <jmarchel100@gmail.com>
2025-08-17 17:19:57 +0700

    Refactor queries to include category data in POSTS_QUERY and POST_QUERY

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,18 +1,57 @@
 import { defineQuery } from "next-sanity";
 
+export const GET_ALL_CATEGORIES = defineQuery(`
+*[_type == "category"] | order(title asc) {
+  _id,
+  title,
+  "slug": slug.current,
+  description,
+}
+`);
+export const POSTS_BY_ALL_CATEGORIES = defineQuery(`
+*[
+  _type=="post" &&
+  count(categories[@->slug.current in $slugs]) == count($slugs)
+]
+| order(publishedAt desc){
+  _id, title, "slug": slug.current, publishedAt,
+  "excerpt": select(
+    defined(excerpt) && excerpt != "" => excerpt,
+    defined(body) => pt::text(body)[0...180],
+    ""
+  ),
+  mainImage{ "url": asset->url, "dims": asset->metadata.dimensions },
+  categories[]->{ title, "slug": slug.current }
+}
+`);
+
+export const POSTS_BY_ANY_CATEGORY = defineQuery(`
+*[
+  _type=="post" &&
+  count(categories[@->slug.current in $slugs]) > 0
+]
+| order(publishedAt desc){
+  _id, title, "slug": slug.current,
+  mainImage{ "url": asset->url, "dims": asset->metadata.dimensions },
+  publishedAt,
+  "excerpt": select(defined(excerpt) && excerpt != "" => excerpt, defined(body) => pt::text(body)[0...180], ""),
+  categories[]->{ title, "slug": slug.current }
+}
+`);
+
 export const POSTS_QUERY =
   defineQuery(`*[_type == "post" && defined(slug.current)]
 | order(publishedAt desc)[0...12]{
   _id,
   title,
   "slug": slug.current,
-  "excerpt": coalesce(excerpt, pt::text(body)[0...180]),
   mainImage{
     ...,
     "url": asset->url,
     "dims": asset->metadata.dimensions
   },
-  publishedAt
+  publishedAt,
+  categories[]->{ title, "slug": slug.current }
 }`);
 
 export const POST_QUERY = defineQuery(`
@@ -28,5 +67,6 @@ export const POST_QUERY = defineQuery(`
   },
   publishedAt,
   myCodeField,
+  categories[]->{ title, "slug": slug.current }
 }
 `);
