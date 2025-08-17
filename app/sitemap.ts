0210d794ce537b534c763e8e14ced6a65218fd78
jimarchel <jmarchel100@gmail.com>
2025-08-17 22:38:59 +0700

    Refactor sitemap function to use consistent lastModified date

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,26 +1,27 @@
-import { getPostSlugs } from "@/lib/get-data";
+import { getBaseUrl, getPostSlugs } from "@/lib/get-data";
 import { MetadataRoute } from "next";
 
 const baseUrl = `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    const baseUrl = getBaseUrl();
   const slugs = await getPostSlugs();
-
+  const now = new Date();
   return [
     {
       url: baseUrl,
-      lastModified: new Date(),
+      lastModified: now,
       changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${baseUrl}/blog`,
-      lastModified: new Date(),
+      lastModified: now,
       changeFrequency: "monthly",
     },
     ...(slugs.map(({ slug }) => ({
       url: `${baseUrl}/blog/${slug}`,
-      lastModified: new Date(),
+      lastModified: now,
       changeFrequency: "daily",
       priority: 1,
     })) as MetadataRoute.Sitemap),
