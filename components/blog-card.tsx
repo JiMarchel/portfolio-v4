5621e024b094ee62bb5858280e07d1d5280edc75
jimarchel <jmarchel100@gmail.com>
2025-08-17 17:18:28 +0700

    Refactor BlogCard component to include categories and improve layout structure

--- components/blog-card.tsx
+++ components/blog-card.tsx
@@ -1,26 +1,34 @@
 "use client";
-import { Card, CardBody } from "@heroui/react";
 import Image from "next/image";
 import Link from "next/link";
+import { Badge } from "./ui/badge";
+import { Card, CardContent } from "./ui/card";
 
 interface BlogCardProps {
   title: string;
   slug: string | null;
   image: string;
   date: string;
+  categories: { title: string | null; slug: string | null }[];
 }
 
-export const BlogCard = ({ title, slug, image, date }: BlogCardProps) => {
+export const BlogCard = ({
+  title,
+  slug,
+  image,
+  date,
+  categories,
+}: BlogCardProps) => {
   return (
-    <Link href={`/blog/${slug}`} className="">
-      <Card className="px-5" >
-        <CardBody className="flex flex-row gap-2 sm:gap-5">
+    <Link href={`/blog/${slug}`}>
+      <Card>
+        <CardContent className="flex flex-row gap-2 sm:gap-5">
           <Image
             src={image}
             alt={title}
             width={500}
             height={300}
-            className="w-auto h-16 object-cover"
+            className="w-auto h-20 object-cover"
           />
           <div>
             <h1 className="sm:text-xl font-medium">{title}</h1>
@@ -31,8 +39,13 @@ export const BlogCard = ({ title, slug, image, date }: BlogCardProps) => {
                 day: "numeric",
               })}
             </p>
+            <div className="space-x-1">
+              {categories.map((c, i) => (
+                <Badge variant="outline" key={i}>{c.title}</Badge>
+              ))}
+            </div>
           </div>
-        </CardBody>
+        </CardContent>
       </Card>
     </Link>
   );
