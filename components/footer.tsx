6ace9f34130da25e33426c635cbc8488a25824f4
jimarchel <jmarchel100@gmail.com>
2025-08-17 17:18:35 +0700

    Refactor Footer component to enhance tooltip implementation and improve icon size

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,31 +1,37 @@
 "use client";
 
-import { Tooltip } from "@heroui/react";
 import { Dock, DockIcon } from "./magicui/dock";
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
 import { Separator } from "./ui/separator";
 import { ThemeSwitch } from "./theme-switch";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
 
 export const Footer = () => {
   return (
-    <Dock className="fixed bottom-3 left-1/2 -translate-x-1/2 sm:px-5 rounded-full z-50">
+    <Dock className="fixed bottom-3 left-1/2 -translate-x-1/2 sm:px-5 rounded-full z-50" >
       {siteConfig.navItems.map((v) => (
         <DockIcon key={v.label}>
-          <Tooltip content={v.label}>
-            <Link href={v.href}>
-              <v.icon className="size-4" />
-            </Link>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href={v.href}>
+                <v.icon className="size-5" />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>{v.label}</TooltipContent>
           </Tooltip>
         </DockIcon>
       ))}
       <Separator orientation="vertical" />
       {siteConfig.social.map((v) => (
         <DockIcon key={v.label}>
-          <Tooltip content={v.label}>
-            <Link href={v.href} target="_blank">
-              <v.icon className="size-4" />
-            </Link>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Link href={v.href} target="_blank">
+                <v.icon className="size-5" />
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent>{v.label}</TooltipContent>
           </Tooltip>
         </DockIcon>
       ))}
