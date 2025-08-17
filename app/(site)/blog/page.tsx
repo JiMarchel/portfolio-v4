c125a1ddb787b45c8c2edc2a59a7ccb96b254031
jimarchel <jmarchel100@gmail.com>
2025-08-17 17:18:16 +0700

    Refactor BlogPage component to support category filtering and improve data fetching logic

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -1,22 +1,73 @@
 import { BlogCard } from "@/components/blog-card";
+import { CategoryParams } from "@/components/category-params";
+import SelectCategory from "@/components/select-category";
+import { Badge } from "@/components/ui/badge";
 import { sanityFetch } from "@/sanity/lib/live";
-import { POSTS_QUERY } from "@/sanity/lib/queries";
+import {
+  GET_ALL_CATEGORIES,
+  POSTS_BY_ALL_CATEGORIES,
+  POSTS_BY_ANY_CATEGORY,
+  POSTS_QUERY,
+} from "@/sanity/lib/queries";
 
-export default async function BlogPage() {
-  const { data } = await sanityFetch({
-    query: POSTS_QUERY,
+type PageProps = {
+  searchParams: Promise<{ c?: string | string[]; mode?: "all" | "any" }>;
+};
+
+function normalizeCats(c?: string | string[]) {
+  if (!c) return [] as string[];
+  const arr = Array.isArray(c) ? c : c.split(",");
+  return arr
+    .map((s) => decodeURIComponent(s).trim().toLowerCase())
+    .filter(Boolean);
+}
+
+export default async function BlogPage({ searchParams }: PageProps) {
+  const { data: categories } = await sanityFetch({
+    query: GET_ALL_CATEGORIES,
   });
+  const requestedSlugs = normalizeCats((await searchParams).c);
+  const validSlugs = new Set(categories.map((c) => c.slug));
+  const filteredSlugs = requestedSlugs.filter((s) => validSlugs.has(s));
+
+  const mode = (await searchParams).mode === "any" ? "any" : "all";
+  const query =
+    filteredSlugs.length === 0
+      ? POSTS_QUERY
+      : mode === "any"
+        ? POSTS_BY_ANY_CATEGORY
+        : POSTS_BY_ALL_CATEGORIES;
+
+  const { data: posts } = await sanityFetch({
+    query,
+    params: filteredSlugs.length ? { slugs: filteredSlugs } : undefined,
+  });
+
   return (
-    <div className="h-full w-fit mx-auto">
-      {data.map((v) => (
-        <BlogCard
-          key={v.slug}
-          title={v.title || "No title"}
-          slug={v.slug}
-          date={v.publishedAt || ""}
-          image={v.mainImage?.url!}
-        />
-      ))}
+    <div className="h-full w-full mx-auto">
+      {/* <CategoryParams />
+
+      <div className="flex flex-wrap gap-1 mb-4">
+        {categories.map((v) => (
+          <Badge key={v._id} title={v.slug!}>
+            {v.title}
+          </Badge>
+        ))}
+      </div> */}
+      <SelectCategory initialTags={categories.map((v) => v.slug!)} />
+
+      <div className="flex flex-col gap-3">
+        {posts.map((v: any) => (
+          <BlogCard
+            key={v.slug}
+            categories={v.categories || []}
+            date={v.publishedAt || ""}
+            image={v.mainImage?.url}
+            slug={v.slug}
+            title={v.title || "No title"}
+          />
+        ))}
+      </div>
     </div>
   );
 }
