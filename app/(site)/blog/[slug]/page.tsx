ded36886f99b2b1175ebc304807c218092459f8d
jimarchel <jmarchel100@gmail.com>
2025-08-17 17:18:20 +0700

    Refactor BlogPage component to clean up imports and enhance image styling

--- app/(site)/blog/[slug]/page.tsx
+++ app/(site)/blog/[slug]/page.tsx
@@ -1,18 +1,12 @@
 import { notFound } from "next/navigation";
-
 import Image from "next/image";
-
 import { POST_QUERY } from "@/sanity/lib/queries";
-
 import { client } from "@/sanity/lib/client";
-
 import RichText from "@/components/rich-text";
-
 import { ChevronLeft } from "lucide-react";
-
 import Link from "next/link";
 
-export const revalidate = 300; // revalidate tiap 5 menit
+export const revalidate = 300; 
 
 export async function generateStaticParams() {
   const slugs: { slug: string }[] = await client.fetch(
@@ -50,7 +44,7 @@ export default async function Page({
           alt={post.title ? post.title : "Cover"}
           width={1200}
           height={630}
-          className="rounded-2xl"
+          className="rounded-2xl w-auto h-auto mb-4 object-cover mx-auto"
         />
       )}
       <div className="mb-10">
